a260bbef915a462f75e64f086cd3ca45f46a61c9
Jan Viñas <mirabetjan@gmail.com>
2021-01-23 10:09:14 +0100

    add debugging commands and prepare tests

--- src/main/java/io/github/janvinas/timetablespawner/TimetableSpawner.java
+++ src/main/java/io/github/janvinas/timetablespawner/TimetableSpawner.java
@@ -1,5 +1,7 @@
 package io.github.janvinas.timetablespawner;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -7,9 +9,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import com.bergerkiller.bukkit.tc.signactions.spawner.SpawnSign;
 import com.bergerkiller.bukkit.common.BlockLocation;
 
-public class TimetableSpawner extends JavaPlugin {
 
-    SpawnSign spawner;
+public class TimetableSpawner extends JavaPlugin {
 
     @Override
     public void onEnable() {
@@ -23,24 +24,25 @@ public class TimetableSpawner extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(command.getName().equalsIgnoreCase("timetablespawner") && args.length == 1){
-            if(args[0].equalsIgnoreCase("register")){
-                Player player = (Player) sender;
-                spawner = new SpawnSign(new BlockLocation(player.getTargetBlock(null, 10).getLocation()));
+
+        if (command.getName().equalsIgnoreCase("timetablespawner")) {
+            Player p = (Player) sender;
+
+            if (args.length == 1 && args[0].equalsIgnoreCase("signinfo")) {
+                SpawnSign targeted = new SpawnSign(new BlockLocation(p.getTargetBlock(null, 10).getLocation()));
+                sender.sendMessage(ChatColor.AQUA + "You are editing the sign:" + targeted.toString());
                 return true;
-            }else if(args[0].equalsIgnoreCase("spawn")){
-                spawner.spawn();
+            }else if(args.length == 5 && args[0].equalsIgnoreCase("locationinfo")){
+                Location location = new Location(getServer().getWorld(args[1]), Double.parseDouble(args[2]), Double.parseDouble(args[3]), Double.parseDouble(args[4]));
+                sender.sendMessage(ChatColor.AQUA + "You are editing the sign:" + new BlockLocation(location).toString());
+                return true;
+            }else if(args.length == 5 && args[0].equalsIgnoreCase("spawn")){
+                Location location = new Location(getServer().getWorld(args[1]), Double.parseDouble(args[2]), Double.parseDouble(args[3]), Double.parseDouble(args[4]));
+                new SpawnSign(new BlockLocation(location)).spawn();
                 return true;
             }
-        }else if(command.getName().equalsIgnoreCase("timetablespawner") && args.length == 2){
-            if(args[0].equalsIgnoreCase("schedule")){
-                if (Integer.parseInt(args [1]) > 0) {
-                    int ticks = Integer.parseInt(args[1]);
-                    getServer().getScheduler().runTaskLater(this, () -> spawner.spawn(), ticks);
-                }
-            }
+
         }
         return false;
     }
-
 }
